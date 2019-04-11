7bb3952337c183a47a6559f747b48ae019786ea3
andrew hu <andrewhu@nyu.edu>
2019-04-11 04:28:15 -0400

    Final

--- karan.py
+++ karan.py
@@ -2,8 +2,11 @@ import time
 import sys
 import termios
 import keyboard
+import serial
+import struct
+import numpy as np
+from xbee import XBee
 
-#define karan
 
 
 def flush_input():
@@ -25,12 +28,12 @@ def speed_down():
 		speed=0
 def turn_left():
 	global angle
-	angle+=1
+	angle+=3
 	if angle>60:
 		angle =60
 def turn_right():
 	global angle
-	angle-=1
+	angle-=3
 	if angle<0:
 		angle =0
 
@@ -46,10 +49,23 @@ def brake_and_reverse():
 	else:
 		speed=-1
 	
+def open_serialComm(port):
+    # Enable USB communication
+    serial_port = serial.Serial(port,9600)
+    #xbee = XBee(serial_port)
+
+    # Refresh the port to be used
+    serial_port.close()
+    serial_port.open()
+
+    print('Serial communication established !!')
+    return serial_port
 
 
+print('\nEstablishing communication with Analyzer Bot....')
+serial_port = open_serialComm('/dev/ttyUSB2')
 while True:#making a loop
-	 #used try so that if user pressed other than the given key error will not be shown
+	#used try so that if user pressed other than the given key error will not be shown
 	if keyboard.is_pressed('w'):	#if key 'a' is pressed
 		speed_up()
 	else:
@@ -64,10 +80,10 @@ while True:#making a loop
 		brake_and_reverse()
 	time.sleep(0.05)
 	print('Speed= '+str(speed)+' Angle = '+str(angle))
-	
-
-
-
+	transmit_speed = int(speed)+1
+	transmit_angle = int(angle)+128
+	serial_port.write(struct.pack('>B', transmit_speed))
+	serial_port.write(struct.pack('>B', transmit_angle))
 
 flush_input()
 
