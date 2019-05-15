698af5724464410e87aa572c401970a2434295d5
Karan Mamaniya <kmamaniya@gmail.com>
2019-05-15 04:14:26 -0400

    Embedded code for indication final

--- NYU_Driverless_Arduino/Indicators/Indicators.ino
+++ NYU_Driverless_Arduino/Indicators/Indicators.ino
@@ -1,16 +1,18 @@
 #include "Indicators.h"
 
-#define Right_Indicator_Pin (0)
-#define Left_Indicator_Pin  (1)
-#define Brake_Light_Pin     (2)
-#define Head_Light_Pin      (3)
-#define Front_Light_Pin     (4)
-#define Autonomous_Mode_Pin (5)
-#define Circuit_Breaker_Pin (6)
+#define Right_Indicator_Pin (4)
+#define Left_Indicator_Pin  (3)
+#define Brake_Light_Pin     (5)
+#define Head_Light_Pin      (2)
+#define Front_Light_Pin     (6)
+#define Autonomous_Mode_Pin (8)
+#define Circuit_Breaker_Pin (7)
 
 Indicator_Status stat;
-bool blink_state = false;
-long previous_time=-500;
+bool left_blink_state = false;
+bool right_blink_state = false;
+long left_previous_time=-500;
+long right_previous_time=-500;
 
 void setup(){
   Serial.begin(9600);
@@ -26,64 +28,78 @@ void setup(){
 uint8_t data;
 
 void loop(){
-  if(Serial.available());
+  if(Serial.available()){
     data = Serial.read();
-  update_status(data,&stat);
-  if(!(stat.Right_Indicator || stat.Right_Indicator))
-      blink_state=false;
-  else if(stat.Right_Indicator){
-      if(millis() - previous_time >= 500){
-          if(blink_state)
-            digitalWrite(Right_Indicator_Pin,LOW);
-          else 
+    update_status(data,&stat);
+  }
+  if(stat.Right_Indicator){
+      if(millis() - right_previous_time >= 500){
+          if(right_blink_state){
             digitalWrite(Right_Indicator_Pin,HIGH);
-
-          previous_time = millis();
+            right_blink_state=false;
+          }
+          else{
+            digitalWrite(Right_Indicator_Pin,LOW);
+            right_blink_state=true;
+          }
+          right_previous_time = millis();
       }
   }
-  else if(stat.Left_Indicator){
-      if(millis() - previous_time >= 500){
-          if(blink_state)
-            digitalWrite(Left_Indicator_Pin,LOW);
-          else 
+  else{
+      digitalWrite(Right_Indicator_Pin,HIGH);
+      right_blink_state=false;
+  }
+  if(stat.Left_Indicator){
+      if(millis() - left_previous_time >= 500){
+          if(left_blink_state){
             digitalWrite(Left_Indicator_Pin,HIGH);
+            left_blink_state=false;
+          }
+          else{
+            digitalWrite(Left_Indicator_Pin,LOW);
+            left_blink_state=true;
+          }
 
-          previous_time = millis();
+          left_previous_time = millis();
       }
+  }
+  else{
+      digitalWrite(Left_Indicator_Pin,HIGH);
+      left_blink_state=false;
   }
 
   if(stat.Brake_Light){
-      digitalWrite(Brake_Light_Pin,HIGH);
+      digitalWrite(Brake_Light_Pin,LOW);
   }
   else 
-      digitalWrite(Brake_Light_Pin,LOW);
+      digitalWrite(Brake_Light_Pin,HIGH);
 
   
   if(stat.Head_Light){
-      digitalWrite(Head_Light_Pin,HIGH);
+      digitalWrite(Head_Light_Pin,LOW);
   }
   else 
-      digitalWrite(Head_Light_Pin,LOW);
+      digitalWrite(Head_Light_Pin,HIGH);
 
   
   if(stat.Front_Light){
-      digitalWrite(Front_Light_Pin,HIGH);
+      digitalWrite(Front_Light_Pin,LOW);
   }
   else 
-      digitalWrite(Front_Light_Pin,LOW);
+      digitalWrite(Front_Light_Pin,HIGH);
       
   if(stat.Autonomous_Mode){
-      digitalWrite(Autonomous_Mode_Pin,HIGH);
+      digitalWrite(Autonomous_Mode_Pin,LOW);
   }
   else 
-      digitalWrite(Autonomous_Mode_Pin,LOW);
+      digitalWrite(Autonomous_Mode_Pin,HIGH);
 
   
   if(stat.Circuit_Breaker){
-      digitalWrite(Circuit_Breaker_Pin,HIGH);
+      digitalWrite(Circuit_Breaker_Pin,LOW);
   }
   else 
-      digitalWrite(Circuit_Breaker_Pin,LOW);
+      digitalWrite(Circuit_Breaker_Pin,HIGH);
 
 
   delay(10);
